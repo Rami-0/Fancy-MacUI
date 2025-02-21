ddedbfdbfd25a93cf1ac8eadaac3449d3392e42e
rami-0 <ramysh11@icloud.com>
2025-02-21 23:09:58 +0600

    14 day

--- src/components/ui/hero-section.tsx
+++ src/components/ui/hero-section.tsx
@@ -6,15 +6,15 @@ const HeroSection: React.FC = () => {
     <section className={'w-full h-full bg-primary'}>
       <Container>
         <div className="w-full h-full pb-24 grid grid-cols-3 grid-rows-3 place-items-stretch relative items-end justify-end">
-          <h1 className="text-6xl z-10 font-bold text-white">Hello, I'm Rami</h1>
-          <h1 className="text-6xl z-10 font-bold text-white">Hello, I'm Rami</h1>
-          <h1 className="text-6xl z-10 font-bold text-white">Hello, I'm Rami</h1>
-          <h1 className="text-6xl z-10 font-bold text-white">Hello, I'm Rami</h1>
-          <h1 className="text-6xl z-10 font-bold text-white">Hello, I'm Rami</h1>
-          <h1 className="text-6xl z-10 font-bold text-white">Hello, I'm Rami</h1>
-          <h1 className="text-6xl z-10 font-bold text-white">Hello, I'm Rami</h1>
-          <h1 className="text-6xl z-10 font-bold text-white">Hello, I'm Rami</h1>
-          <h1 className="text-6xl z-10 font-bold text-white">Hello, I'm Rami</h1>
+          <span className="grid place-content-center h-full text-6xl z-10 font-bold text-white">1</span>
+          <span className="grid place-content-center h-full text-6xl z-10 font-bold text-white">2</span>
+          <span className="grid place-content-center h-full text-6xl z-10 font-bold text-white">3</span>
+          <span className="grid place-content-center h-full text-6xl z-10 font-bold text-white">4</span>
+          <span className="grid place-content-center h-full text-6xl z-10 font-bold text-white">5</span>
+          <span className="grid place-content-center h-full text-6xl z-10 font-bold text-white">6</span>
+          <span className="grid place-content-center h-full text-6xl z-10 font-bold text-white">7</span>
+          <span className="grid place-content-center col-span-2 h-full text-6xl z-10 font-bold text-white">8</span>
+          {/*<span className="grid place-content-center h-full text-6xl z-10 font-bold text-white">9</span>*/}
         </div>
       </Container>
     </section>
