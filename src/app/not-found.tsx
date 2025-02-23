5859319dafe795dd18c60266431d466f490b55c0
rami-0 <ramysh11@icloud.com>
2025-02-23 21:43:00 +0600

    15 day

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,12 @@
-import FuzzyText from "@/components/blocks/TextAnimations/FuzzyText";
+'use client';
+import FuzzyText from '@/components/blocks/TextAnimations/FuzzyText';
 
 export default async function NotFound() {
-    return (
-        <FuzzyText baseIntensity={0.2} hoverIntensity={0.5} enableHover={true}>
-            404
-        </FuzzyText>
-    )
+  return (
+    <div className={'w-full h-full flex items-center justify-center'}>
+      <FuzzyText baseIntensity={0.2} hoverIntensity={0.5} enableHover={true}>
+        404
+      </FuzzyText>
+    </div>
+  );
 }
